343d1d8a9f3586452952976d36480c4ebdf8fb3e
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-30 21:35:07 -0300

    feat: add api to translate phrase

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { config } from '@gluestack-ui/config';
 import {  Box, CloseIcon, CopyIcon, GluestackUIProvider, RepeatIcon, ShareIcon, StarIcon, Text, View } from '@gluestack-ui/themed';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
@@ -16,7 +16,36 @@ const Home = () => {
 const Container = () => {
 
   const [text, setText] = useState();
-  const [textTranlated, setTextTranlated] = useState();
+  const [textTranslated, setTextTranslated] = useState();
+  const [langCurrent, setLangCurrent] = useState("")
+  const [langRequest, setLangRequest] = useState("")
+
+  async function submitForm() {
+    console.log(text);
+    setLangCurrent("pt")
+    setLangRequest("en")
+    const res = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${langCurrent}|${langRequest}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const data = await res.json();
+    
+    // Verifique se a tradução foi bem-sucedida
+    if (data.responseStatus === 200) {
+        setTextTranslated(data.responseData.translatedText);
+    } else {
+        console.error("Erro na tradução:", data.responseDetails);
+    }
+
+    console.log(data);
+}
+
+  useEffect(() => {
+      submitForm();
+  }, [text])
 
   return (
     <View display='flex' justifyContent='space-between' h={'$full'}> 
@@ -26,22 +55,22 @@ const Container = () => {
         </View>
 
         {/* Main */}
-        <View display='flex' flexDirection='column' alignItems='center'>
+        <View display='flex' flexDirection='column' alignItems='center' marginBottom={100}>
           {/* Idiomas */}
             <View bgColor='#E3E3E3' width={"75%"} borderRadius={50} height={50} display='flex' flexDirection='row' alignItems='center' justifyContent='space-around'>
-                <Text fontSize={20}>English</Text>
-                  <RepeatIcon></RepeatIcon>
                 <Text fontSize={20}>Portuguese</Text>
+                  <RepeatIcon></RepeatIcon>
+                <Text fontSize={20}>English</Text>
             </View>
 
-            <View bgColor='#E3E3E3' width={"75%"} height={216} borderRadius={16} marginTop={20} justifyContent='space-between' padding={10}>
+            <View bgColor='#E3E3E3' width={"75%"} height={150} borderRadius={16} marginTop={20} justifyContent='space-between' padding={10}>
                 <View display='flex' >
                   <View paddingHorizontal={10} display='flex' flexDirection='row' justifyContent='space-between'>
-                    <Text fontSize={25}>English</Text>
+                    <Text fontSize={25}>Português</Text>
                     <CloseIcon width={20} height={20}></CloseIcon>
                   </View>
                   <View paddingHorizontal={20} paddingTop={16}>
-                    <TextInput style={{fontSize: 17}} placeholder='Enter text here...' onChangeText={(value) => {setText(value)}}></TextInput>
+                    <TextInput style={{fontSize: 17}} placeholder='Enter text here...' onChangeText={(value) => {setText(value);} }></TextInput>
                   </View>
                 </View>
                 <View paddingHorizontal={10} alignItems='flex-end'>
@@ -56,10 +85,10 @@ const Container = () => {
             <View bgColor='#E3E3E3' width={"75%"} height={216} borderRadius={16} marginTop={20} justifyContent='space-between' padding={10}>
                 <View display='flex' >
                   <View paddingHorizontal={10} display='flex' flexDirection='row' justifyContent='space-between'>
-                    <Text fontSize={25}>Spanish</Text>
+                    <Text fontSize={25}>Inglês</Text>
                   </View>
                   <View paddingHorizontal={20} paddingTop={16}>
-                    <Text fontSize={17}>{textTranlated}</Text>
+                    <Text fontSize={17}>{textTranslated}</Text>
                   </View>
                 </View>
                 <View padding={10} display='flex' flexDirection='row' alignSelf='flex-end' gap={20}>
